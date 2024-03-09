a80c2c6842a47cd03cb48b0703f7eb902d8eed51
Steven-DeYoung <none>
2024-03-08 17:19:47 -0800

    Added tests for dataTableBuilder.js

--- src/dataTableBuilder.js
+++ src/dataTableBuilder.js
@@ -2,32 +2,47 @@ import { TableMaker } from './tableMaker';
 
 /**
  * Constructs an HTML table from the passed data.
- * @param rowNames array of row names (excluding header row)
- * @param colNames array of column names (including the row name column)
+ * @param rowNames Array of row names (excluding header row).
+ * @param colNames Array of column names (including the row name column).
  * @param dataset Array of data points (primatives, or objects having the same properties).
  */
 export function buildDataTable(rowNames, colNames, dataset) {
-    rowNames = rowNames.slice(); // Don't modify the passed array
-    let tableHeader = TableMaker.constructHeaderRow(colNames);
-    let tableRows = [];
+    rowNames = rowNames?.slice(); // Don't modify the passed array
+    let tableHeader = null;
+    let numColumns = 0;
+    if (colNames != null) {
+        tableHeader = TableMaker.constructHeaderRow(colNames);
+        numColumns = colNames.length;
+    }
 
-    for (let dataPoint of dataset.data) {
+    let dataArr = [];
+    if (dataset != null) {
+        dataArr = dataset;
+    }
+
+    let tableRows = [];
+    for (let dataPoint of dataArr) {
         let rowData = [];
 
         if (typeof dataPoint === "object") {
             for (let key in dataPoint) {
                 rowData.push(dataPoint[key]);
+
+                 // Skip over any 'extra' data (only applies when numColumns > 0)
+                if (rowData.length === numColumns) {
+                    break;
+                }
             }
         } else {
             rowData.push(dataPoint);
         }
 
-        // Create empty cells for any extra header columns (excluding the row label)
-        for (let i = rowData.length; i < colNames.length - 1; i++) {
+        // Create empty cells for any extra header columns
+        for (let i = rowData.length; i < numColumns; i++) {
             rowData.push("");
         }
 
-        tableRows.push(TableMaker.constructRow(rowNames.shift(), rowData));
+        tableRows.push(TableMaker.constructRow(rowNames?.shift(), rowData));
     }
     return TableMaker.constructTable(tableHeader, tableRows);
 }
--- test/tableMaker.test.js
+++ test/tableMaker.test.js
@@ -6,37 +6,37 @@ test("constructEmptyRow", () => {
     let result = TableMaker.constructRow(null, null);
     let expectedRow = '<tr></tr>';
     expect(result).toEqual(expectedRow);
-})
+});
 
 test("constructRowWithLabelOnly", () => {
     let result = TableMaker.constructRow("label", null);
     let expectedRow = '<tr><th scope="row">label</th></tr>';
     expect(result).toEqual(expectedRow);
-})
+});
 
 test("constructRowWithEmptyData", () => {
     let result = TableMaker.constructRow("label", []);
     let expectedRow = '<tr><th scope="row">label</th></tr>';
     expect(result).toEqual(expectedRow);
-})
+});
 
 test("constructRowWithDataOnly", () => {
     let result = TableMaker.constructRow(null, ["data"]);
     let expectedRow = '<tr><td>data</td></tr>';
     expect(result).toEqual(expectedRow);
-})
+});
 
 test("constructRowSingleData", () => {
     let result = TableMaker.constructRow("label", ["data"]);
     let expectedRow = '<tr><th scope="row">label</th><td>data</td></tr>';
     expect(result).toEqual(expectedRow);
-})
+});
 
 test("constructRowMultipleData", () => {
     let expectedRow = '<tr><th scope="row">label</th><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>';
     let result = TableMaker.constructRow("label", ["1","2","3","4","5"]);
     expect(result).toEqual(expectedRow);
-})
+});
 
 
 // Tests for constructHeaderRow()
@@ -44,19 +44,19 @@ test("constructEmptyHeaderRow", () => {
     let result = TableMaker.constructHeaderRow(null);
     let expectedRow = '<tr></tr>';
     expect(result).toEqual(expectedRow);
-})
+});
 
 test("constructHeaderRowWithEmptyColumns", () => {
     let result = TableMaker.constructHeaderRow([]);
     let expectedRow = '<tr></tr>';
     expect(result).toEqual(expectedRow);
-})
+});
 
 test("constructHeaderRowSingleColumn", () => {
     let result = TableMaker.constructHeaderRow(["header"]);
     let expectedRow = '<tr><th scope="col">header</th></tr>';
     expect(result).toEqual(expectedRow);
-})
+});
 
 test("constructHeaderRowMultipleColumn", () => {
     let result = TableMaker.constructHeaderRow(["header1","header2", "header3", "header4", "header5"]);
@@ -64,14 +64,14 @@ test("constructHeaderRowMultipleColumn", () => {
         '<th scope="col">header2</th><th scope="col">header3</th>' +
         '<th scope="col">header4</th><th scope="col">header5</th></tr>';
     expect(result).toEqual(expectedRow);
-})
+});
 
 // Tests for constructTable()
 test("constructEmptyTable", () => {
     let result = TableMaker.constructTable(null, null);
     let expectedTable = '<table></table>';
     expect(result).toEqual(expectedTable);
-})
+});
 
 
 test("constructTableWithHeaderOnly", () => {
@@ -79,7 +79,7 @@ test("constructTableWithHeaderOnly", () => {
     let result = TableMaker.constructTable(headerRow, null);
     let expectedTable = `<table><thead>${headerRow}</thead></table>`;
     expect(result).toEqual(expectedTable);
-})
+});
 
 
 test("constructTableWithEmptyRows", () => {
@@ -87,7 +87,7 @@ test("constructTableWithEmptyRows", () => {
     let result = TableMaker.constructTable(headerRow, []);
     let expectedTable = `<table><thead>${headerRow}</thead></table>`;
     expect(result).toEqual(expectedTable);
-})
+});
 
 
 test("constructTableRowsOnly", () => {
@@ -95,7 +95,7 @@ test("constructTableRowsOnly", () => {
     let result = TableMaker.constructTable(null, rows);
     let expectedTable = `<table><tbody>${rows[0]}</tbody></table>`;
     expect(result).toEqual(expectedTable);
-})
+});
 
 
 test("constructTableSingleRow", () => {
@@ -104,7 +104,7 @@ test("constructTableSingleRow", () => {
     let result = TableMaker.constructTable(headerRow, rows);
     let expectedTable = `<table><thead>${headerRow}</thead><tbody>${rows[0]}</tbody></table>`;
     expect(result).toEqual(expectedTable);
-})
+});
 
 
 test("constructTableMultipleRows", () => {
@@ -119,5 +119,5 @@ test("constructTableMultipleRows", () => {
     let expectedTable = `<table><thead>${headerRow}</thead>` +
         `<tbody>${rows[0]}${rows[1]}${rows[2]}${rows[3]}${rows[4]}</tbody></table>`;
     expect(result).toEqual(expectedTable);
-})
+});
 
